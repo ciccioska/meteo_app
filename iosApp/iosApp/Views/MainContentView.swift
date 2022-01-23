af4d2465cc125b58ae0e47c4a0f0895bde1b5045
Francesco Scalise <me@francescoscalise.info>
2022-01-23 10:25:03 +0100

    Changed: changes project structure

--- iosApp/iosApp/Views/MainContentView.swift
+++ iosApp/iosApp/Views/MainContentView.swift
@@ -11,7 +11,7 @@ import shared
 
 struct MainContentView: View {
     @State private var country: Country = Country(name: "Italy")
-    @State private var nationalState: NationState = NationState(name: "Lazio")
+    @State private var nationalState: NationState = NationState(name: "Calabria")
     @State private var city: City? = nil
     
     var sdk: AirVisualSDK = AirVisualSDK()
@@ -22,18 +22,20 @@ struct MainContentView: View {
                 Text("Meteo App 1.0")
                     .font(.largeTitle)
                     .fontWeight(.medium)
-                    .foregroundColor(Color.blue)
+                    .foregroundColor(Color.green)
+                
+                Spacer()
                 
                 Text("Select country")
                     .font(.headline)
                     .fontWeight(.regular)
-                    .foregroundColor(Color.red)
+                    .foregroundColor(Color.blue)
                 NavigationLink(destination: CountrySelectionView(selectedCountry: $country, viewModel: CountrySelectionView.ViewModel.init(sdk: sdk))) {
                     Text(country.name)
                         .frame(minWidth: 0, maxWidth: 300)
                         .padding()
                         .foregroundColor(.white)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .leading, endPoint: .trailing))
                         .cornerRadius(15)
                         .font(.title)
                 }
@@ -41,13 +43,13 @@ struct MainContentView: View {
                 Text("Select nation state")
                     .font(.headline)
                     .fontWeight(.regular)
-                    .foregroundColor(Color.red)
+                    .foregroundColor(Color.blue)
                 NavigationLink(destination: NationalStateSelectionView(selectedNationalState: $nationalState, viewModel:  NationalStateSelectionView.ViewModel.init(sdk: sdk, country: country))) {
                     Text(nationalState.name)
                         .frame(minWidth: 0, maxWidth: 300)
                         .padding()
                         .foregroundColor(.white)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .leading, endPoint: .trailing))
                         .cornerRadius(15)
                         .font(.title)
                 }
@@ -55,13 +57,15 @@ struct MainContentView: View {
                 Text("Select city")
                     .font(.headline)
                     .fontWeight(.regular)
-                    .foregroundColor(Color.red)
+                    .foregroundColor(Color.blue)
+                    .frame(alignment: .leading)
+                    
                 NavigationLink(destination: CitySelectionView(selectedCity: $city, viewModel:  CitySelectionView.ViewModel.init(sdk: sdk, country: country, nationState: nationalState ))) {
-                    Text(city?.name ?? "Select city")
+                    Text(city?.name ?? "-")
                         .frame(minWidth: 0, maxWidth: 300)
                         .padding()
                         .foregroundColor(.white)
-                        .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .leading, endPoint: .trailing))
                         .cornerRadius(15)
                         .font(.title)
                 }
@@ -72,11 +76,13 @@ struct MainContentView: View {
                             .frame(minWidth: 0, maxWidth: 300)
                             .padding()
                             .foregroundColor(.white)
-                            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                            .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .leading, endPoint: .trailing))
                             .cornerRadius(15)
                             .font(.title)
                     }
                 }
+                
+                Spacer()
             }
         }
     }
